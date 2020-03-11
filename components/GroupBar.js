030dde650b75c4fedb4975f5c2016d0680465254
faisal <faisal.ksa.505@hotmail.com faisal.ksa.505@hotmail.com>
2020-03-11 18:05:21 -0400

    increment and decrement

--- components/GroupBar.js
+++ components/GroupBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {View, TouchableOpacity, Text, StyleSheet,Button } from 'react-native';
 import{withNavigation} from "react-navigation"
 
 
@@ -7,6 +7,15 @@ class GroupBar extends Component{
     constructor(props){
         super(props)
     }
+    state={
+        count:0
+      }  
+      incrementValue=() => {
+      this.setState({count:this.state.count+1})
+      }
+      decrementValue=() => {
+      this.setState({count:this.state.count-1})
+      }
 
     render(){
     
@@ -24,6 +33,7 @@ class GroupBar extends Component{
                 <View style={styles.body_container}>
                     <View style={styles.left_container}>
                         <Text style ={styles.body_text}>{this.props.group_name}</Text>
+                        {/* <Text style ={styles.body_text}>{this.props.group_Description}</Text> */}
                         <Text style ={styles.body_text}>{this.props.interests.join(" ")}</Text>
                     </View>
 
@@ -31,6 +41,24 @@ class GroupBar extends Component{
                         <TouchableOpacity style={styles.join_button} onPress={() => this.props.navigation.navigate('Message',{id: this.props.id})}>
                             <Text style ={styles.join_text}>Join</Text>
                         </TouchableOpacity>
+                            
+                            
+                            {/* adding 
+                            like and dislike buttons
+
+                             */}
+                        <TouchableOpacity style={styles.like_button} onPress={ this.incrementValue}>
+                            <Text>like</Text> 
+                            
+                            </TouchableOpacity> 
+                           
+                            <Text style={{fontSize:19,color:"red"}}>{this.state.count}</Text>
+                        <TouchableOpacity style={styles.dislike_button} onPress={ this.decrementValue}>
+                            <Text>Dislike</Text>
+                            </TouchableOpacity>
+
+                            
+                               
                     </View>
                 </View>
             </View>
@@ -87,6 +115,19 @@ const styles = StyleSheet.create({
         color:"gold",
         fontWeight:"bold",
         fontSize:15
+    },
+    dislike_button:{
+        backgroundColor:"red",
+        padding:5,
+        borderRadius:10,
+        color:"gold",
+    },
+    like_button:{
+        marginTop:10,
+        backgroundColor:"white",
+        padding:5,
+        borderRadius:10,
+        color:"gold",
     }
 
 
