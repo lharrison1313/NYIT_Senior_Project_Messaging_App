61df9fc096f69f3e9a363b6e6ff31cce84949d12
Luke Harrison <35813094+lharrison1313@users.noreply.github.com>
2020-03-11 22:20:27 -0400

    fixed buttons

--- components/GroupBar.js
+++ components/GroupBar.js
@@ -27,38 +27,30 @@ class GroupBar extends Component{
             >
                
                 <View style={styles.header_container}>
-                        <Text style = {{flex: 1}}>{this.props.location}</Text>
-                    <Text style = {{flex: 1}}>{this.props.date.toString()}</Text>
+                    <Text style = {{flex: .75, fontSize:12, fontWeight:"bold"}}>{this.props.location}</Text>
+                    <Text style = {{flex: .25, fontSize:12, fontWeight:"bold"}}>{this.props.date.toString()}</Text>
                 </View>
                 <View style={styles.body_container}>
                     <View style={styles.left_container}>
-                        <Text style ={styles.body_text}>{this.props.group_name}</Text>
+                        <Text style ={{flex:.50}}>{this.props.group_name}</Text>
                         {/* <Text style ={styles.body_text}>{this.props.group_Description}</Text> */}
-                        <Text style ={styles.body_text}>{this.props.interests.join(" ")}</Text>
+                        <Text style ={{flex:.50}}>{this.props.interests.join(" ")}</Text>
                     </View>
 
-                    <View styles={styles.right_container}>
+                    <View style={styles.right_container}>
                         <TouchableOpacity style={styles.join_button} onPress={() => this.props.navigation.navigate('Message',{id: this.props.id})}>
                             <Text style ={styles.join_text}>Join</Text>
                         </TouchableOpacity>
-                            
-                            
-                            {/* adding 
-                            like and dislike buttons
-
-                             */}
-                        <TouchableOpacity style={styles.like_button} onPress={ this.incrementValue}>
-                            <Text>like</Text> 
-                            
+                        <View style = {styles.ld_container} >
+                            <TouchableOpacity style={styles.like_button} onPress={ this.incrementValue}>
+                                <Text>Like</Text>  
                             </TouchableOpacity> 
-                           
-                            <Text style={{fontSize:19,color:"red"}}>{this.state.count}</Text>
-                        <TouchableOpacity style={styles.dislike_button} onPress={ this.decrementValue}>
-                            <Text>Dislike</Text>
+                            <Text style={{fontSize:19,color:"black"}}>{this.state.count}</Text>
+                            <TouchableOpacity style={styles.dislike_button} onPress={ this.decrementValue}>
+                                <Text>Dislike</Text>
                             </TouchableOpacity>
-
-                            
-                               
+                        </View>
+                        
                     </View>
                 </View>
             </View>
@@ -72,62 +64,62 @@ const styles = StyleSheet.create({
     bar_container:{
         flexDirection:'column',
         backgroundColor: '#00BED6',
-        height: 100,
-        padding:10,
-
-        borderColor:"purple",
-
+        height: 120,
+        padding: 10,
+        borderColor:"black",
         borderWidth: 1
-
     },
 
     header_container:{
         flexDirection:'row',
-        marginBottom:5
+        flex: .30
     },
 
-    body_text:{
-        marginBottom:10
-    },
     left_container:{
         flexDirection:"column",
-       flex:.75
-       
+        
+        flex: .55
     },
     right_container:{
-        flexDirection:"column",
+        flexDirection:"row",
         alignItems:"center",
         justifyContent:"center",
-        flex:.25
+        flex: .45,
 
     },
     body_container:{
         flexDirection:"row",
-        flex:1
+        flex: .70,
+        
+
     },
     join_button:{
-        backgroundColor:"blue",
+        flex: .20,
+        backgroundColor:"grey",
         padding:5,
         borderRadius:10,
+        marginHorizontal: 2
     },
 
     join_text:{
-        color:"gold",
         fontWeight:"bold",
-        fontSize:15
+        fontSize:14
     },
+
+    ld_container:{
+        flex: .80,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
     dislike_button:{
         backgroundColor:"red",
-        padding:5,
-        borderRadius:10,
-        color:"gold",
+        padding:3
     },
     like_button:{
-        marginTop:10,
-        backgroundColor:"white",
-        padding:5,
-        borderRadius:10,
-        color:"gold",
+        backgroundColor:"green",
+        padding:3
     }
 
 
