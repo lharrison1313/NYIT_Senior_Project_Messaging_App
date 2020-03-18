209296d8f57dfb72849a2c84d3777b1d22bf7de7
faisal <faisal.ksa.505@hotmail.com faisal.ksa.505@hotmail.com>
2020-03-18 18:32:31 -0400

    pod install, google map update and node moduels

--- ios/NYIT_Messaging_App/AppDelegate.m
+++ ios/NYIT_Messaging_App/AppDelegate.m
@@ -10,8 +10,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import GooglePlaces; 
-#import GoogleMaps;
+@import GooglePlaces; 
+@import GoogleMaps;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
