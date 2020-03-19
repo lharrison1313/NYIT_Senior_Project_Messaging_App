26af8b65f649e3583fc174c91e12df98b8134878
faisal <faisal.ksa.505@hotmail.com faisal.ksa.505@hotmail.com>
2020-03-19 00:34:23 -0400

    change styling to forget password

--- components/ForgetPasswordScreen.js
+++ components/ForgetPasswordScreen.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import auth from '@react-native-firebase/auth';
-import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, Alert,TouchableOpacity} from 'react-native';
 import{withNavigation} from "react-navigation"
 // import * as firebase from 'firebase';
 
-import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 class ForgetPasswordScreen extends Component{
@@ -24,21 +24,22 @@ class ForgetPasswordScreen extends Component{
             });
     }
 
-    onBackToLoginPress = () => {
-        var navActions = NavigationActions.reset({
-            index: 0,
-            actions: [NavigationActions.navigate({routeName: "Login"})]
-        });
-        this.props.navigation.dispatch(navActions);
-    }
+    // onBackToLoginPress = () => {
+    //     var navActions = NavigationActions.reset({
+    //         index: 0,
+    //         actions: [NavigationActions.navigate({routeName: "Login"})]
+    //     });
+    //     this.props.navigation.dispatch(navActions);
+    // }
+    
 
     render() {
         return (
-            <View style={{paddingTop:50, alignItems:"center"}}>
+            <View style={styles.login_container}>
 
-                <Text>Forgot Password</Text>
+                <Text style={styles.text}>Forgot Password</Text>
 
-                <TextInput style={{width: 200, height: 40, borderWidth: 1}}
+                <TextInput style={styles.field}
                     value={this.state.email}
                     onChangeText={(text) => { this.setState({email: text}) }}
                     placeholder="Email"
@@ -47,8 +48,11 @@ class ForgetPasswordScreen extends Component{
                     autoCorrect={false}
                 />
 
-                <Button title="Reset Password" onPress={this.onResetPasswordPress} />
+                <TouchableOpacity  style = {styles.button} onPress={this.onResetPasswordPress}>
+                <Text>Reset Password </Text>
+                </TouchableOpacity>
                 <Button title="Back to Login..." onPress={this.onBackToLoginPress} />
+                
             </View>
         );
     }
@@ -56,7 +60,37 @@ class ForgetPasswordScreen extends Component{
 
 const styles = StyleSheet.create({
    
-
+    button:{
+        backgroundColor: '#00BED6',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 100,
+        height: 50,
+        width: 300,
+        margin: 10
+    },
+    field:{
+        width: 200, height: 40, 
+        borderWidth: 1,
+        marginVertical: 5, 
+        backgroundColor: "lightgrey", 
+        width:300
+    },
+    login_container:{
+        flex:1,
+        backgroundColor: '#5F6362',
+        alignItems:"center",
+        justifyContent: "center"
+    },
+    text:{
+        color:"black",
+        fontWeight:"bold",
+         backgroundColor: '#00BED6',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 300,
+          
+    }
 
 })
 
