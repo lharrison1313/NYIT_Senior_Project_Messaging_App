8368da16231c16f39d6864daa7a125bab8763c1e
faisal <faisal.ksa.505@hotmail.com faisal.ksa.505@hotmail.com>
2020-04-24 20:58:55 -0400

    added image picker
    added image picker

--- ui/screens/ProfileScreen.js
+++ ui/screens/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView ,TouchableOpacity} from 'react-native';
 import {getUserInfo, getCurrentUserID} from "../../api/MessagingAppAPI"
 import OvalButton from "../components/OvalButton";
 import {signOut} from '../../api/MessagingAppAPI';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ImagePicker from 'react-native-image-picker';
 import { AppStyles, color_c } from '../styles/AppStyles';
 
 
@@ -13,9 +14,28 @@ export default class ProfileScreen extends Component{
         this.state = {
             name: "Default Name",
             status: "Online",
+            groupImageSource:""
+
         }
     }
-
+    imagebuttonHandler = ()=>{
+        ImagePicker.showImagePicker((response)=>{
+          console.log('Response =',response);
+          if (response.didCancel) {
+            console.log('User cancelled image picker');
+          } else if (response.error) {
+            console.log('ImagePicker Error: ', response.error);
+          } else if (response.customButton) {
+            console.log('User tapped custom button: ', response.customButton);
+          } else {
+            const source = { uri: response.uri };
+ 
+            this.setState({
+                groupImageSource: source,
+                });
+            }
+        });
+    }
     componentDidMount(){
        getUserInfo(getCurrentUserID(), this.userInfoRetrieved)
     }
@@ -30,7 +50,15 @@ export default class ProfileScreen extends Component{
             <SafeAreaView style={{flex:1}}>
             <View style = {AppStyles.screen}>
                 <View style = {styles.content_container}>
-                    <Icon name="user" size={100} color={color_c}/>
+
+                    {/* <Icon name="user" size={100} color={color_c}/> */}
+                <TouchableOpacity
+               
+               onPress = {this.imagebuttonHandler}
+               >
+                   <Icon name="user" size={100} color={color_c} />
+               </TouchableOpacity>
+
 
                     <Text style = {{margin: 10,}}>
                         {this.state.name}
