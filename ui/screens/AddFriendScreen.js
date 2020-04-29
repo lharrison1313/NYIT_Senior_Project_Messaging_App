628f5286e922eb1194c4772e0b83342dce4d9dc1
AbdulAli92 <47880456+AbdulAli92@users.noreply.github.com>
2020-04-29 18:29:20 -0400

    Create AddFriendScreen.js

---
+++ ui/screens/AddFriendScreen.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import{withNavigation} from "react-navigation"
+import { FlatList, TextInput} from 'react-native-gesture-handler';
+import {getUsers} from "../../api/MessagingAppAPI"
+import FriendBar from "../components/FriendBar"
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const plus = <Icon name="plus-circle" size={40} color="#00BED6" />;
+
+class AddFriendScreen extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { 
+            friendList: []
+        };
+    }
+
+    componentDidMount(){
+        getUsers(this.retrieveUsers)
+        .then((unsub) => {
+            this.unsubscribe = unsub
+            console.log("subscribe")})
+        .catch((error)=> console.log("FriendScreen: ",error))
+    }
+
+    componentWillUnmount(){
+        if(this.unsubscribe != null){
+            console.log("unsubscribe")
+            this.unsubscribe()
+        }
+    }
+
+    retrieveUsers = (users) => {
+        this.setState({
+            friendList: users
+        });
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={{flex:1}}>
+            <View style ={styles.container}>
+
+                <View style={styles.header_container}>
+                    <TextInput 
+                    style = {styles.search_bar}
+                    onChangeText = {(input)=>{this.textChanged(input)}}
+                    placeholder = {"Search"}
+                    />
+                </View>
+
+                <FlatList
+                    data = {this.state.friendList}
+                    renderItem={({ item }) => (
+                        <FriendBar
+                            name = {item.UserName} 
+                            id = {item.id}
+                            navigation = {this.props.navigation}
+                            //interests = {item.Interests}
+                        />
+                        )}
+                    keyExtractor = {item => item.id}
+                />
+
+                
+            </View>
+
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    header_container:{
+        flexDirection:"row",
+        alignItems: "center",
+        padding:15
+    },
+
+    container:{
+        flex:1,
+        backgroundColor:"grey"
+    },
+
+    new_friend_button:{
+        flex:.15,
+        backgroundColor: "grey",
+        justifyContent: "center",
+        alignItems: "center",
+        height:40,
+        width: 40,
+        borderRadius: 20,
+        marginHorizontal:5
+    },
+
+    search_bar:{
+        flex:.85,
+        height:40, 
+        backgroundColor: "white",
+        borderRadius:30,
+        marginHorizontal:5
+    }
+    
+
+})
+
+
+export default withNavigation(FriendScreen)
