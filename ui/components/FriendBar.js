a913f8187cfb2253a531574f8d7b08f05a26d2b1
AbdulAli92 <47880456+AbdulAli92@users.noreply.github.com>
2020-04-29 18:31:37 -0400

    Create FriendBar.js

---
+++ ui/components/FriendBar.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import {View, TouchableOpacity, Text, StyleSheet, TouchableHighlightBase } from 'react-native';
+import {addUserToFriend,getCurrentUserID,removeUserFromFriend, getUserInfo} from '../../api/MessagingAppAPI';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class FriendBar extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            
+        }  
+    }
+
+    handleAddFriend = () =>{
+        addUserToFriend(getCurrentUserID(),this.props.id)
+    }
+
+    handleRemoveFriend = () =>{
+        removeUserFromFriend(getCurrentUserID(), this.props.id)
+    }
+
+    handleNavigateToInfoScreen = () =>{
+        
+    }
+
+    render(){
+    
+        return(
+            
+            <TouchableOpacity style = {styles.bar_container} onPress = {() => this.NavigateToInfoScreen()}>
+                <View style = {{flexDirection:"row", flex:1 }}>
+
+                    <View style={styles.left_container}>
+                        <Icon name="user" size={80} color="white"/>
+                            
+                    </View>
+
+                    <View style={styles.mid_container}>
+
+                        <Text style ={{flex:.50, fontSize:25, fontWeight: "bold"}}>{this.props.name}</Text>
+                        <Text style ={{flex:.50}}>{this.props.interests}</Text>
+                    
+                    </View>
+
+                    <View style={styles.right_container}>
+                        <TouchableOpacity style={styles.add_button} onPress={() => this.handleAddFriend()}>
+                            <Text style ={styles.add_text}>Add</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.remove_button} onPress={() => this.handleRemoveFriend}>
+                            <Text style ={styles.add_text}>Remove</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    
+                
+                </View>
+            </TouchableOpacity>
+            
+        );
+    }
+
+}
+
+
+const styles = StyleSheet.create({
+
+    left_container:{
+        flexDirection:"column",
+        flex: .30,
+    },
+
+    mid_container:{
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+        flex: .60,
+
+    },
+
+    right_container:{
+        flexDirection:"column",
+        flex: .30,
+
+    },
+
+    add_button:{
+        flex: .50,
+        flexDirection:"column",
+        backgroundColor:"grey",
+        padding:5,
+        borderRadius:10,
+        alignItems:"center",
+        justifyContent: "center"
+    },
+
+    remove_button:{
+        flex: .50,
+        flexDirection:"column",
+        backgroundColor:"grey",
+        padding:5,
+        borderRadius:10,
+        alignItems:"center",
+        justifyContent: "center"
+    },
+
+    bar_container:{
+        flexDirection:'column',
+        backgroundColor: '#00BED6',
+        height: 100,
+        padding: 10,
+        borderColor:"grey",
+        borderBottomWidth: 1
+    },
+})
